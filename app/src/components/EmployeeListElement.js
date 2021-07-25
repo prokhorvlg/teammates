a346200295916264fa76eca4f4afbda4f6dd97f1
Valentin Sigalov <vlgstudent036@gmail.com>
2021-07-25 13:16:19 -0400

    Responsive updates

--- app/src/components/EmployeeListElement.js
+++ app/src/components/EmployeeListElement.js
@@ -10,7 +10,7 @@ const EmployeeListElement = ({ employee, selectedEmployee, setSelectedEmployee, 
       <button onClick={() => { setSelectedEmployee(employee.id); setSelectedScreen(1); }} className={(employeeIsSelected) ? "selected" : ""}>
         <img src={employee.picture.thumbnail} alt={employee.name.first + " " + employee.name.last} />
         <p className="employee-name">{employee.name.first} {employee.name.last}</p>
-        <p className="employee-title">Front End Developer</p>
+        <p className="employee-title desktop-only">Front End Developer</p>
         <FontAwesomeIcon icon={faArrowRight} className="fa-arrow-icon" />
       </button>
     </li>
